288f7afdf5bb8484180a49648579f7c7520ffa51
wiktkaj492 <72332485+wiktkaj492@users.noreply.github.com>
2022-12-29 19:47:30 +0100

    SeedData prototype

--- CinemaAPI/CinemaAPI/CinemaAPI/Models/SeedData.cs
+++ CinemaAPI/CinemaAPI/CinemaAPI/Models/SeedData.cs
@@ -1,16 +1,45 @@
 using CinemaAPI.Controllers;
 
-namespace CinemaAPI.Models;
 
-public class SeedData
+namespace CinemaAPI.Models
 {
-    public static void Initialize(CinemaDbContext context)
+    public class SeedData
     {
-        if (context.Screenings.Any() || context.Rooms.Any() || context.Locations.Any())
+        public static void Initialize(CinemaDbContext context)
         {
-            return;
-        }
-        //TODO - add seed data
+            if (context.Rooms.Any() || context.Screenings.Any() || context.Locations.Any())
+            {
+                return;
+            }
 
-}
+            var miechow = new Location()
+            {
+                id_location = Guid.NewGuid(),
+                city = "Miechów"
+            };
+
+            context.Rooms.AddRange(
+
+                new Room()
+                {
+                    id_location = miechow.id_location,
+                    id_room = Guid.NewGuid(),
+                    room_number = 1,
+                    row = 6,
+                    column = 6,
+                    taken_seats = " ",
+                    unavailable_seats = " "
+                }
+
+              );
+
+            context.Locations.AddRange(
+
+                miechow
+
+
+                );
+
+        }
+    }
 }
