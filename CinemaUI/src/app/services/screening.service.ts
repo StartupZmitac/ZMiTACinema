6d18f44b4d87183e450a4b2cf9044fa779ffa324
Aleksandra Kyc <ola.kyc@gmail.com>
2022-12-30 22:39:39 +0100

    screening service

--- CinemaUI/src/app/services/screening.service.ts
+++ CinemaUI/src/app/services/screening.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { Screening } from '../models/screening.model';
+import { Location } from '../models/location.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,15 @@ export class ScreeningService {
 
   constructor(private http: HttpClient) { }
 
-  getScreenings(): Observable<Screening[]>{
+  getAllScreenings(): Observable<Screening[]>{
     return this.http.get<Screening[]>(this.baseApiUrl+'/api/Screening')
+  }
+
+  getScreenings(date: Date, location: Location): Observable<Screening[]>{
+
+    let url = this.baseApiUrl+'/api/Screening';
+    let params= new HttpParams();
+    params.append(location.city,date.toString());
+    return this.http.get<Screening[]>(url,{params})
   }
 }
