707e412c0996b8d53aa3b8de341d5e4a82ef3bf0
Szymon <szymko3615@protonmail.com>
2023-01-06 13:31:10 +0100

    Added getCurrentTime and checkTime methods

--- CinemaAPI/CinemaAPI/CinemaAPI/Controllers/Validator.cs
+++ CinemaAPI/CinemaAPI/CinemaAPI/Controllers/Validator.cs
@@ -1,6 +1,36 @@
+using System.Globalization;
+using System.Net;
+
 namespace CinemaAPI.Controllers;
 
 public class Validator
 {
+
     
+    public static DateTime getCurrentTime()
+    {
+        try{
+            using (var response = 
+                   WebRequest.Create("http://www.google.com").GetResponse())
+                return DateTime.ParseExact(response.Headers["date"],
+                    "ddd, dd MMM yyyy HH:mm:ss 'GMT'",
+                    CultureInfo.InvariantCulture.DateTimeFormat,
+                    DateTimeStyles.AssumeUniversal);
+        }
+        catch (WebException)
+        {
+            return DateTime.Now; //In case something goes wrong. 
+        }
+    }
+
+
+    public bool checkTime(DateTime fromFront)
+    {
+        if (DateTime.Compare(getCurrentTime(), fromFront) <= 0)
+            return true;
+        return false;
+    }
+
+
+
 }
