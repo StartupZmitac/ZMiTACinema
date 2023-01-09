5f47a56343e0552b578e95bd0cd179ea5761de31
Michał Niedbalski <michnie507@student.polsl.pl>
2023-01-09 15:50:09 +0100

    Improved main-page, so same film names arent repeated.

--- CinemaUI/src/app/components/main-page/main-page.component.ts
+++ CinemaUI/src/app/components/main-page/main-page.component.ts
@@ -13,13 +13,15 @@ export class MainPageComponent implements OnInit {
   time: Date = new Date();
   localizationName: string | null | undefined;
   screenings: Screening[] = [];
+  currentFilm: string | null | undefined;
 
   constructor(private service: ScreeningService, private route: ActivatedRoute, private router: Router) {
   }
 
   ngOnInit(): void {
     this.localizationName = this.route.snapshot.paramMap.get('localizationName');
-    this.getScreenings()
+    this.getScreenings();
+
   }
 
   onHourClick(event: Event, _room: number, location: string){
@@ -28,21 +30,33 @@ export class MainPageComponent implements OnInit {
         room: _room.toString()
       }});
   }
+  setCurrentFilm(film: string) {
+    this.currentFilm = film;
+  }
 
   getScreenings(){
     if(this.localizationName!= undefined)
-    this.service.getScreenings("2023-01-07", this.localizationName)
+    this.service.getScreenings("2023-01-08", this.localizationName)
     .subscribe({
       next: (screenings) =>{
         this.screenings = screenings;
         console.log("got screenings")
         console.log(screenings.length)
+        this.screenings = this.screenings.sort((a, b) => {
+          if (a.film < b.film) {
+            return -1;
+          } else if (a.film > b.film) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
       },
       error: (response) => {
         console.log(response);
       }
-    }
-    )
+    })
+    console.log(this.screenings);
   }
 }
 
