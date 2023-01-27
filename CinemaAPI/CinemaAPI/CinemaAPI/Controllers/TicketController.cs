777be5840944c05e33c773223c6f49eaecb8c1e7
Szymon <szymko3615@protonmail.com>
2023-01-27 22:04:25 +0100

    Activated checkTime method

--- CinemaAPI/CinemaAPI/CinemaAPI/Controllers/TicketController.cs
+++ CinemaAPI/CinemaAPI/CinemaAPI/Controllers/TicketController.cs
@@ -154,7 +154,7 @@ namespace CinemaAPI.Controllers
             }
 
             Validator validator = new Validator();
-            if (validator.checkColumnRowOutOfRange(ticketSeat, connectedRoom.column, connectedRoom.row) == false)
+            if (validator.checkColumnRowOutOfRange(ticketSeat, connectedRoom.column, connectedRoom.row) == false || validator.checkTime(connectedScreening.Time) == false)
             {
                 return false;
             }
