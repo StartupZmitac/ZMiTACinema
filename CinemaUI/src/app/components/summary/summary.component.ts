613d3a8090fc04484de240225b1096d80998598f
Aleksandra Kyc <ola.kyc@gmail.com>
2023-01-29 21:34:55 +0100

    guzik

--- CinemaUI/src/app/components/summary/summary.component.ts
+++ CinemaUI/src/app/components/summary/summary.component.ts
@@ -17,7 +17,7 @@ export class SummaryComponent implements OnInit {
   normal = 0
   reduced = 0
   price = 0
-
+  show = false
   constructor(private cookieService: CookieService, private tservice: TicketService, private router:Router, private route: ActivatedRoute) { 
     //pass transaction id from buy/reserve
   }
@@ -55,6 +55,32 @@ export class SummaryComponent implements OnInit {
         this.errorMessage = "there was an error"
       }
     }
+  }
 
+  refreshTicket(){
+    if(this.ticketID){
+    try{
+      this.tservice.getTransaction(this.ticketID).subscribe(data=>{
+        data.forEach(element => {
+          this.seats+=element.seat+' '
+          if(element.type=="reduced"){
+            this.reduced+=1
+            this.price+=15}
+          else{
+            this.normal+=1
+            this.price+=30.5
+          }
+          this.show = true
+        });
+        if(data.length==0)
+          this.errorMessage = "This ticket doesn't exist"
+        }
+        
+        )
+      }
+      catch(err){
+        this.errorMessage = "there was an error"
+      }
+    }
   }
 }
