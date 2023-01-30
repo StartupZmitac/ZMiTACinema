7f86a39b6701fbfb416f62e2dcca960b2595dd6d
Szymon <szymko3615@protonmail.com>
2023-01-30 17:14:57 +0100

    login works

--- CinemaUI/src/app/components/user-login/user-login.component.ts
+++ CinemaUI/src/app/components/user-login/user-login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { AdminService } from 'src/app/services/admin.service';
 import {LocationService} from "../../services/location.service";
 
 
@@ -9,15 +12,27 @@ import {LocationService} from "../../services/location.service";
 
 export class LoginComponent implements OnInit {
 
-  constructor(){
+  username = ''
+  password = ''
+
+  constructor(private aservice: AdminService, private router: Router, private cookieService: CookieService){
 
   }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    
   }
     
   onSubmit(){
-    
+
+      this.aservice.authenticate(this.username, this.password).subscribe(
+        data=>{
+          console.log(data)
+          this.router.navigate(['/admin-page'])
+          this.cookieService.set('admin', "true")
+        }
+      )
+
   }
 
 }
